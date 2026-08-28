3f4b59effad27bce8fb170ca51b11cee2c7b4121
Myrat204209 <myrat.annamuhammedov2002@gmail.com>
2026-08-28 13:47:49 +0500

    Initial Commit

---
+++ BarcodeScanner/BarcodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeScannerApp.swift
+//  BarcodeScanner
+//
+//  Created by Azat Japarov on 28.08.26.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
