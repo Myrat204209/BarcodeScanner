8042f804cea331ed33efb84e6602eb6b77c9e71d
Myrat204209 <myrat.annamuhammedov2002@gmail.com>
2026-08-28 14:54:47 +0500

    Barcode scanner app using UIKit

---
+++ BarcodeScanner/ScannerView.swift
@@ -0,0 +1,18 @@
+//
+//  ScannerView.swift
+//  BarcodeScanner
+//
+//  Created by Azat Japarov on 28.08.26.
+//
+
+import SwiftUI
+
+struct ScannerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ScannerView()
+}
